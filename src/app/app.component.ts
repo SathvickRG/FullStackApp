eaae756f838f16e448ee336e2dc1516b5fcc6570
Satghvick <sathvick2016@gmail.com>
2021-04-13 15:55:15 -0400

    header created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
- // template: '<h1> {{title}}<\h1>',
   styleUrls: ['./app.component.css']
   
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
     WelcomeComponent,
     LoginComponent,
     ErrorComponent,
-    ListTodosComponent
+    ListTodosComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +27,7 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
     FormsModule,
     ReactiveFormsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
