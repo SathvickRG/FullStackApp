ebd0d2e4b549dae5b8a721530dca824b9f4577bb
Satghvick <sathvick2016@gmail.com>
2021-04-19 16:37:35 -0400

    hardcoded authenticate service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 import { WelcomeComponent } from '../welcome/welcome.component';
 
 @Component({
@@ -16,21 +17,25 @@ export class LoginComponent implements OnInit {
 
   //Router
   //Dependency Injection
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+  private HardcodedAuthenticationService: HardcodedAuthenticationService) 
+  { }
+  
 
   ngOnInit(): void {
   }
  
   handleLogin()
   {
-    // console.log(this.username);
+        // invalid credential check
+  // if(this.username==="sathvick" && this.password==="dummy")
 
-    // invalid credential check
-    if(this.username === "sathvick" && this.password === 'dummy')
+  if(this.HardcodedAuthenticationService.authenticate(this.username,this.password))
     {
-      this.router.navigate(['Welcome', this.username]) //route to specific page
-     
-      this.invalidLogin = false }
+      //redirect to welcome page
+      this.router.navigate(['welcome', this.username]) //route to specific page
+      this.invalidLogin = false 
+    }
     else {
        this.invalidLogin = true}
   }
