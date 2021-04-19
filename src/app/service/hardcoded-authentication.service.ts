f3d91db5144d1f1c6bab374ebdbb6c64a41adc47
Satghvick <sathvick2016@gmail.com>
2021-04-19 16:37:10 -0400

    Added authenticate service

---
+++ src/app/service/hardcoded-authentication.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HardcodedAuthenticationService {
+
+  constructor() { }
+  
+//method to authneticate login username and password
+  authenticate(username:string, password:string)
+    {
+      console.log('before ' + this.isUserLoggedIn());
+    if(username === "sathvick" && password === 'dummy')
+    {
+      sessionStorage.setItem('authenticateUser', username);
+      console.log('after ' + this.isUserLoggedIn());
+      return true;
+    }
+    return false;
+  }
+  isUserLoggedIn()
+  {
+    let user = sessionStorage.getItem('authenticateUser')
+    return !(user === null)
+  }
+}
