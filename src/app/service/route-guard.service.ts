4fa8cf120f1eceb8a9dc2e19684544a527696e17
Satghvick <sathvick2016@gmail.com>
2021-05-17 14:09:01 -0400

    Implemented Route Guard Service

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService {
+export class RouteGuardService implements CanActivate {
 
-  constructor() { }
+
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+  
+  //method to activate when user is logged in
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log("Reached Route guard");
+    if (this.hardcodedAuthenticationService.isUserLoggedIn())
+      return true;
+
+    else return false;
+  }
+
 }
+
